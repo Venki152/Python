72cef832532def0b99a1a3b893b81d9498a91827
Venkat Yatam <61537586+Venki152@users.noreply.github.com>
2020-08-27 22:00:16 -0500

    Python_1
    First Code of python

---
+++ app2.py
@@ -0,0 +1,16 @@
+print("helloworld")
+print("*" * 20)
+age = 28
+
+message = "eligible" if age >=20 else "not eligible"
+
+print(message)
+
+for i in "python":
+    print(i)
+
+
+
+for i in range(0, 15):
+    if i % 2 != 0:
+        print(i)
