9584f0ed63422b392b938599c44799ff117dff64
Venkat Yatam <61537586+Venki152@users.noreply.github.com>
2020-09-13 11:34:34 -0500

    Create Yatam_V_Unite3.py
    Create a list with the following sports (in the given order):
      Cricket, Badminton, Squash, Racquetball, Tennis
    
    Display the list
    Find the length of of the list and print this number
    Replace the third element (i.e. index 2) in the list with Football
    Display the list

---
+++ Yatam_V_Unite3.py
@@ -0,0 +1,16 @@
+# Unit 3 lists lab
+
+sportsL = ["Cricket", "Badminton", "Squash", "Racquetball", "Tennis"]
+
+print(sportsL)
+
+l = len(sportsL)
+
+print(l)
+r = sportsL[2]
+print(r)
+sportsL.insert(2,"Football")
+sportsL.pop(3)
+
+
+print(sportsL)
