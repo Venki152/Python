241fef84df285cc84747c57b36e97536059aeb44
Venkat Yatam <61537586+Venki152@users.noreply.github.com>
2020-09-13 22:57:14 -0500

    Update Yatam_V_Unit3.py
    Added 1b code

--- Yatam_V_Unit3.py
+++ Yatam_V_Unit3.py
@@ -1,16 +1,44 @@
-# Unit 3 lists lab
+# Unit 3 1a lists lab
 
 sportsL = ["Cricket", "Badminton", "Squash", "Racquetball", "Tennis"]
 
 print(sportsL)
 
 l = len(sportsL)
-
-print(l)
-r = sportsL[2]
-print(r)
-sportsL.insert(2,"Football")
-sportsL.pop(3)
+print('Length of the list is', l)
+sportsL[2] = "Football" #replace index 2 element with Football
+print('Below is the updated list\n',sportsL)
 
 
-print(sportsL)
+# sportsL.insert(2,"Football")
+# sportsL.pop(3)
+
+
+#r = sportsL[2]
+#print(r)
+#sportsL.replace(r, "Football")
+
+
+
+# ------------------ Lab 1b
+
+#import statistics as st
+NumList = [2, 3, 4, 88, 525, 345, 1, 16]  #create NumList
+sum = 0  #initiate sum with 0
+
+for i in NumList:
+    sum = sum + i
+
+avg = sum/ len(NumList)     # average by diving total sum with number of elements
+print(avg)
+
+NumList.remove(525)
+print(NumList)
+
+sum = 0         #set sum to 0 as we recalculate for the elements of list
+for i in NumList:
+    sum = sum + i
+
+avg = sum/ len(NumList)     # average by diving total sum with number of elements
+print(round(avg,2))
+
