a5d8cd4245aa3914cc0ba9cb5b1337a4ca019adb
Venkat Yatam <61537586+Venki152@users.noreply.github.com>
2020-09-25 20:25:24 -0500

    Create Yatam_V_Unit4.py

---
+++ Yatam_V_Unit4.py
@@ -0,0 +1,20 @@
+
+
+
+
+# if statements lab 4
+MyList = ['Deca', 'Hecto', 'Kilo', 'Mega', 'Tera', 'Pico', 'Nano']  #defined list
+Test = 'Alpha' #defined Test variable
+
+if Test in MyList:
+    print("'%s'IN the list" %Test)
+else:
+    print("'%s' NOT IN the list" %Test)
+
+Test = 'Hecto' #change value of Test variable
+
+if Test in MyList:
+    print("'%s'IN the list" %Test)
+else:
+    print("'%s' NOT IN the list" %Test)
+
